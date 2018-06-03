5e49833acec82b28b7f9100ab00a8470489cdf50
HO <1155106609@link.cuhk.edu.hk>
2018-06-03 23:02:41 +0800

    uploading all on lec 242

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+
+const app = new Clarifai.App({
+ apiKey: 'a3a4e04c11b64d318a3dacf1292ac7a9'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with API'));
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json('unable to get entries'));
+};
+
+module.exports = {
+	handleImage,
+	handleApiCall
+};
